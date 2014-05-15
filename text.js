bcc9ef586db725ca332e299eb6ee55fcfb1e6d78
guybedford <guybedford@gmail.com>
2014-05-14 17:45:17 -0700

    upgrade to new plugin format

--- text.js
+++ text.js
@@ -1,18 +1,6 @@
 /*
   Text plugin
 */
-module.exports = function(name, address, fetch, callback, errback) {
-  fetch(address, function(text) {
-    callback('module.exports = "'
-    + text
-      .replace(/(["\\])/g, '\\$1')
-      .replace(/[\f]/g, "\\f")
-      .replace(/[\b]/g, "\\b")
-      .replace(/[\n]/g, "\\n")
-      .replace(/[\t]/g, "\\t")
-      .replace(/[\r]/g, "\\r")
-      .replace(/[\u2028]/g, "\\u2028")
-      .replace(/[\u2029]/g, "\\u2029")
-    + '";');
-  }, errback);
+exports.instantiate = function(load) {
+  return load.source;
 }
