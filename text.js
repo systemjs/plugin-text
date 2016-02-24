cc0fdb4de7ece41bb335a892f09d330c86c433aa
guybedford <guybedford@gmail.com>
2016-02-24 17:56:18 +0200

    revert esm output

--- text.js
+++ text.js
@@ -12,11 +12,12 @@ exports.translate = function(load) {
     .replace(/[\u2028]/g, "\\u2028")
     .replace(/[\u2029]/g, "\\u2029");
 
-  if(System.transpiler === false) {
+  if(System.transpiler === false || true) {
     load.metadata.format = 'amd';
     return 'def' + 'ine(function() {\nreturn "' + text + '";\n});';
   }
 
+  // disabled pending rollout of https://github.com/systemjs/builder/commit/13ed8f896ab2df733252f75d8ed770c2d9e11862
   load.metadata.format = 'esm';
   return 'export default "' + text + '";';
 }
