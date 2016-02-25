87a7bc2e4dc8f6787b681abf669438190ff62e25
guybedford <guybedford@gmail.com>
2016-02-25 13:35:15 +0200

    just use json.stringify

--- text.js
+++ text.js
@@ -2,22 +2,12 @@
   Text plugin
 */
 exports.translate = function(load) {
-  var text = load.source
-    .replace(/(["\\])/g, '\\$1')
-    .replace(/[\f]/g, "\\f")
-    .replace(/[\b]/g, "\\b")
-    .replace(/[\n]/g, "\\n")
-    .replace(/[\t]/g, "\\t")
-    .replace(/[\r]/g, "\\r")
-    .replace(/[\u2028]/g, "\\u2028")
-    .replace(/[\u2029]/g, "\\u2029");
-
-  if(System.transpiler === false || true) {
+  if (System.transpiler === false || true) {
     load.metadata.format = 'amd';
-    return 'def' + 'ine(function() {\nreturn "' + text + '";\n});';
+    return 'def' + 'ine(function() {\nreturn ' + JSON.stringify(load.source) + ';\n});';
   }
 
   // disabled pending rollout of https://github.com/systemjs/builder/commit/13ed8f896ab2df733252f75d8ed770c2d9e11862
   load.metadata.format = 'esm';
-  return 'export default "' + text + '";';
+  return 'export default ' + JSON.stringify(load.source) + ';';
 }
