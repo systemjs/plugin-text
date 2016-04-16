1b5cc9c48dd99ea28c171aea312e384a97165e0e
npbenjohnson <npbenjohnson@gmail.com>
2016-04-15 17:02:46 -0700

    enabled esm export code
    this was disabled pending rollout of systemjs/builder@13ed8f8

--- text.js
+++ text.js
@@ -2,12 +2,11 @@
   Text plugin
 */
 exports.translate = function(load) {
-  if (System.transpiler === false || true) {
+  if (System.transpiler === false) {
     load.metadata.format = 'amd';
     return 'def' + 'ine(function() {\nreturn ' + JSON.stringify(load.source) + ';\n});';
   }
-
-  // disabled pending rollout of https://github.com/systemjs/builder/commit/13ed8f896ab2df733252f75d8ed770c2d9e11862
+  
   load.metadata.format = 'esm';
   return 'export default ' + JSON.stringify(load.source) + ';';
 }
