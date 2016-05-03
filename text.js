f6ba200e810789fdaa73827857ae0576cc3d7612
guybedford <guybedford@gmail.com>
2016-05-03 08:11:56 +0200

    only use esm for builds

--- text.js
+++ text.js
@@ -2,11 +2,11 @@
   Text plugin
 */
 exports.translate = function(load) {
-  if (System.transpiler === false) {
-    load.metadata.format = 'amd';
-    return 'def' + 'ine(function() {\nreturn ' + JSON.stringify(load.source) + ';\n});';
+  if (this.builder && this.transpiler) {
+    load.metadata.format = 'esm';
+    return 'export default ' + JSON.stringify(load.source) + ';';
   }
   
-  load.metadata.format = 'esm';
-  return 'export default ' + JSON.stringify(load.source) + ';';
+  load.metadata.format = 'amd';
+  return 'def' + 'ine(function() {\nreturn ' + JSON.stringify(load.source) + ';\n});';
 }
